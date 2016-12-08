bc95c19790ee1382b5cb59b1fb9d85d75d356dc0
dhanendras <dhanendra.chaudhary@gmail.com>
2016-12-08 21:02:03 +0530

    asd

--- Server_Side/blockchain/participants/participants.js
+++ Server_Side/blockchain/participants/participants.js
@@ -1,42 +1,42 @@
 /*eslint-env node */
-var create = require(__dirname+'/CRUD/bpccreate.js');
+var create = require(__dirname+'/CRUD/create.js');
 exports.create = create.create;
 
 var read = require(__dirname+'/CRUD/read.js');
 exports.read = read.read;
 
-var minersFile = require(__dirname+'/miners/bpmminers.js');
+var minersFile = require(__dirname+'/miners/miners.js');
 var miners = {};
 miners.read = minersFile.read;
 exports.miners = miners;
 
-var distributorsFile = require(__dirname+'/distributors/bpddistributors.js');
+var distributorsFile = require(__dirname+'/distributors/distributors.js');
 var distributors = {};
 distributors.read = distributorsFile.read;
 exports.distributors = distributors;
 
 
-var dealershipsFile = require(__dirname+'/dealerships/bpddealerships.js');
+var dealershipsFile = require(__dirname+'/dealerships/dealerships.js');
 var dealerships = {};
 dealerships.read = dealershipsFile.read;
 exports.dealerships = dealerships;
 
-var buyersFile = require(__dirname+'/buyers/bpbbuyers.js');
+var buyersFile = require(__dirname+'/buyers/buyers.js');
 var buyers = {};
 buyers.read = buyersFile.read;
 exports.buyers = buyers;
 
-var tradersFile = require(__dirname+'/traders/bpttraders.js');
+var tradersFile = require(__dirname+'/traders/traders.js');
 var traders = {};
 traders.read = tradersFile.read;
 exports.traders = traders;
 
-var cuttersFile = require(__dirname+'/cutters/bpccutters.js');
+var cuttersFile = require(__dirname+'/cutters/cutters.js');
 var cutters = {};
 cutters.read = cuttersFile.read;
 exports.cutters = cutters;
 
-var jewellery_makersFile = require(__dirname+'/jewellery_makers/bpjjewellery_makers.js');
+var jewellery_makersFile = require(__dirname+'/jewellery_makers/jewellery_makers.js');
 var jewellery_makers = {};
 jewellery_makers.read = jewellery_makersFile.read;
 exports.jewellery_makers = jewellery_makers;
