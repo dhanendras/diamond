c7b07ae9cf0e4843823f0f5964ecf5d26d584a0c
dhanendras <dhanendra.chaudhary@gmail.com>
2016-12-08 22:22:02 +0530

    sd

--- Server_Side/blockchain/participants/participants.js
+++ Server_Side/blockchain/participants/participants.js
@@ -5,12 +5,12 @@ exports.create = create.create;
 var read = require(__dirname+'/CRUD/read.js');
 exports.read = read.read;
 
-var minersFile = require(__dirname+'/miners/miners.js');
+var minersFile = require(__dirname+'/miner/miner.js');
 var miners = {};
 miners.read = minersFile.read;
 exports.miners = miners;
 
-var distributorsFile = require(__dirname+'/distributors/distributors.js');
+var distributorsFile = require(__dirname+'/distributor/distributor.js');
 var distributors = {};
 distributors.read = distributorsFile.read;
 exports.distributors = distributors;
@@ -21,27 +21,27 @@ var dealerships = {};
 dealerships.read = dealershipsFile.read;
 exports.dealerships = dealerships;
 
-var buyersFile = require(__dirname+'/buyers/buyers.js');
+var buyersFile = require(__dirname+'/buyer/buyer.js');
 var buyers = {};
 buyers.read = buyersFile.read;
 exports.buyers = buyers;
 
-var tradersFile = require(__dirname+'/traders/traders.js');
+var tradersFile = require(__dirname+'/trader/trader.js');
 var traders = {};
 traders.read = tradersFile.read;
 exports.traders = traders;
 
-var cuttersFile = require(__dirname+'/cutters/cutters.js');
+var cuttersFile = require(__dirname+'/cutter/cutter.js');
 var cutters = {};
 cutters.read = cuttersFile.read;
 exports.cutters = cutters;
 
-var jewellery_makersFile = require(__dirname+'/jewellery_makers/jewellery_makers.js');
+var jewellery_makersFile = require(__dirname+'/jewellery_maker/jewellery_maker.js');
 var jewellery_makers = {};
 jewellery_makers.read = jewellery_makersFile.read;
 exports.jewellery_makers = jewellery_makers;
 
-var customersFile = require(__dirname+'/customers/customers.js');
+var customersFile = require(__dirname+'/customer/customer.js');
 var customers = {};
 customers.read = customersFile.read;
 exports.customers = customers;
